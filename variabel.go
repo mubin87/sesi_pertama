b4e54e5217cb137e890c410788309cc7b0021d16
mubin87 <frans.andra.boys87@gmail.com>
2022-03-21 11:42:32 +0700

    sesi 1 golang

---
+++ variabel.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var nama = "Golang"
+	// var age = 5
+	// var address = "Google"
+
+	// fmt.Printf("Halo namaku %s, umurku %d, dan aku berasal dari %s \n", nama, age, address)
+
+	// fmt.Println("ini adalah bahasa pemrograman ==>", nama)
+	// fmt.Println("saya sudah berumur ==>", age)
+	// fmt.Println("saya berasal dari ==>", address)
+
+	// var name string
+	// var age int = 23
+
+	// name = "Golang"
+	// age = 5
+
+	// fmt.Println("ini adalah bahasa pemrograman ==>", name)
+	// fmt.Println("saya sudah berumur ==>", age)
+
+	// var name = "Golang"
+	// var age = 5
+
+	// fmt.Printf("%T, %T", name, age)
+
+	// var student1, student2, student3 string = "satu", "dua", "tiga"
+
+	// var first, second, third int
+
+	// first, second, third = 1, 2, 3
+
+	// fmt.Println(student1, student2, student3)
+
+	// fmt.Println(first, second, third)
+
+	// var firstVariable string
+
+	// var name, age, address = "Golang", 5, "Google"
+
+	// _, _, _, _ = firstVariable, name, age, address
+
+	// fmt.Println(firstVariable, name, age)
+
+	name := "Android"
+	age := 20
+
+	fmt.Printf("%T, %T", name, age)
+}
